388d334a4537645a7cd3c4091eb1b55c5ce1a90a
noodlebatteries124 <koland423@gmail.com>
2026-07-30 13:29:14 +0800

    fixed callisto theme

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ const playlist = [
     { title: "ECHO(full ver.)／東雲彰人(Shinonome Akito)×白石杏(Shiraishi An)｜Project Sekai", src: "songs/【プロセカ】ECHO(full ver.)／東雲彰人(Shinonome Akito)×白石杏(Shiraishi An)｜Project Sekai｜Color Coded Lyrics [l04FfoADnyw].mp3" },
     { title: "Tokinoko City - 40mp (Covered by Yachiyo)", src: "songs/【歌ってみた】トリノコシティ – 40mP ⧸ covered by 月見ヤチヨ(cv.早見沙織) from 超かぐや姫！ [gsGjcLVI6X4].mp3" },
     { title: "Vs. Susie - Toby Fox", src: "songs/Vs. Susie [cGuDpoJNImA].mp3" },
-    { title: "Canto 9 - Callisto theme", src: "songs/星降る海 [hqx09wf56PM].mp3songs/Limbus Company OST - Canto IX Boss 3 Battle Theme 2 [BuzAgy_88HM].mp3" }
+    { title: "Canto 9 - Callisto theme", src: "songs/Limbus Company OST - Canto IX Boss 3 Battle Theme 2 [BuzAgy_88HM].mp3" }
 ];
 
 let currentTrackIndex = 0;
